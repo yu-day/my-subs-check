ebad7f9b33a7cb369096368d471f02ce5a2f3391
yuday <yuday0421@gmail.com>
2026-05-05 17:15:15 +0800

    Add files via upload

--- check/platform/alive.go
+++ check/platform/alive.go
@@ -3,7 +3,7 @@ package platform
 import (
 	"net/http"
 
-	"github.com/beck-8/subs-check/config"
+	"github.com/yu-day/my-subs-check/config"
 )
 
 func CheckAlive(httpClient *http.Client) (bool, error) {
