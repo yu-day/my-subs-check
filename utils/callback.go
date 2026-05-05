3f11d6f447ecf85e9d342244ad0fea2f7682c93b
yuday <yuday0421@gmail.com>
2026-05-05 17:23:16 +0800

    Add files via upload

--- utils/callback.go
+++ utils/callback.go
@@ -9,7 +9,7 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/beck-8/subs-check/config"
+	"github.com/yu-day/my-subs-check/config"
 )
 
 // ExecuteCallback 执行回调脚本
--- utils/notify.go
+++ utils/notify.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/beck-8/subs-check/config"
+	"github.com/yu-day/my-subs-check/config"
 )
 
 // NotifyRequest 定义发送通知的请求结构
